f3559e794c042dbef2a5fb651b96fe1d5f9b0858
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2024-10-22 18:00:37 +0300

    Remove TouchEvent listener from handleClickOutside
    - Fixed a typo in Settings page

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -6,9 +6,8 @@ const useClickOutside = (
 	childRef?: RefObject<HTMLElement>
 ) => {
 	const handleClickOutside = useCallback(
-		(event: MouseEvent | TouchEvent) => {
+		(event: MouseEvent) => {
 			// Return if main or child ref contains the clicked element
-
 			if (mainRef.current && mainRef.current.contains(event.target as Node)) return;
 			if (childRef && childRef.current && childRef.current.contains(event.target as Node)) return;
 
@@ -20,11 +19,9 @@ const useClickOutside = (
 
 	useEffect(() => {
 		document.addEventListener("mousedown", handleClickOutside);
-		// document.addEventListener("touchstart", handleClickOutside);
 
 		return () => {
 			document.removeEventListener("mousedown", handleClickOutside);
-			// document.removeEventListener("touchstart", handleClickOutside);
 		};
 	}, [handleClickOutside]);
 };
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -32,7 +32,7 @@ const Settings = () => {
 					<p className="mb-2">
 						All your files and account data will be permanently deleted.{" "}
 						<br className="hidden sm:inline-block" />
-						You might be propmted to reauthenticate during the process. <br />{" "}
+						You might be prompted to reauthenticate during the process. <br />{" "}
 						<br className="inline-block sm:hidden" />
 						This action is irreversible.
 					</p>
